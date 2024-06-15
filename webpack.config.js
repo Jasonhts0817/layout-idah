dacc0498c59141958586ddeb1077be15b9e786a1
Jason Huang <jason.hts0817@gmail.com>
2024-06-15 11:29:36 +0800

    feat: modify webpack setting

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
 
 const isProduction = process.env.NODE_ENV == "production";
 const isLocal = process.env.NODE_ENV == "local";
@@ -12,33 +12,34 @@ const isLocal = process.env.NODE_ENV == "local";
 const config = {
   entry: () => {
     let outPutFile = {
-      main: "./src/index.js",
       idah: "./src/scss/idah.scss",
-      'idah-rwd': "./src/scss/idah-rwd.scss",
-      'idah-service': "./src/scss/idah-service.scss",
-      'idah-solution': "./src/scss/idah-solution.scss",
-      'search-dialogs': "./src/scss/search-dialogs.scss",
-
+      "idah-rwd": "./src/scss/idah-rwd.scss",
+      "idah-service": "./src/scss/idah-service.scss",
+      "idah-solution": "./src/scss/idah-solution.scss",
+      "search-dialogs": "./src/scss/search-dialogs.scss",
     };
     let localFile = {
-      'develop-css': "./src/scss/develop-css.scss",
-    }
+      "develop-css": "./src/scss/develop-css.scss",
+    };
     outPutFile = isLocal ? { ...outPutFile, ...localFile } : outPutFile;
     return outPutFile;
-  }
-  ,
+  },
   optimization: {
     removeEmptyChunks: true,
   },
   output: {
     clean: true,
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    host: "localhost"
+    host: "localhost",
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: "index.html", minify: false, inject: isLocal }),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      minify: false,
+      inject: isLocal,
+    }),
     new MiniCssExtractPlugin({ filename: "css/[name].css" }),
     new RemoveEmptyScriptsPlugin(),
     new CopyPlugin({
@@ -47,8 +48,8 @@ const config = {
         { from: "src/fonts", to: "./fonts" },
         { from: "src/images", to: "./images" },
         { from: "src/js", to: "./js" },
-      ]
-    })
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -58,16 +59,18 @@ const config = {
           { loader: MiniCssExtractPlugin.loader },
           { loader: "css-loader", options: { url: false } },
           { loader: "postcss-loader" },
-          { loader: 'sass-loader', options: { sassOptions: { outputStyle: "expanded" } }, }
+          {
+            loader: "sass-loader",
+            options: { sassOptions: { outputStyle: "expanded" } },
+          },
         ],
-      }
+      },
     ],
   },
-  target: 'web'
+  target: "web",
 };
 
 if (isLocal) {
-
 }
 module.exports = () => {
   console.log(process.env.NODE_ENV);
